0a0957466c717a5f193762738f1625e7fdb0655c
Zaid-k1 <zaidkeylany@gmail.com>
2025-10-26 14:26:05 +0300

    This is the first commit for GitTest2

---
+++ Guessing Game.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/*
+int main () {
+	
+	int secretNumber = 6;
+	int guess;
+	int lives=3;
+	
+	printf("guess a number between 1 and 10 (you got %d lives)\n", lives);
+	
+	while(guess!=secretNumber && lives != 0) {
+	scanf("%d", &guess);
+	lives--;
+	if (guess==secretNumber) printf("You Won!");
+	else if (lives == 0) printf("you ran out of lives");
+	else if (lives == 1) printf("You lost, try again :/ (you have %d life left)\n", lives);
+	else printf("You lost, try again :/ (you have %d lives left)\n", lives);
+	
+	}
+
+	return 0; 
+}*/
